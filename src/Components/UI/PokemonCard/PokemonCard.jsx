ba5bbb60f88a8ff03f46cb170a77c6a6840a19da
armendariz4 <javierarmendariz1.com>
2023-05-13 14:44:27 +0200

    last steps

--- src/Components/UI/PokemonCard/PokemonCard.jsx
+++ src/Components/UI/PokemonCard/PokemonCard.jsx
@@ -1,4 +1,7 @@
 import "./styles.css";
+import weight from "../../../assets/images/icons/width.svg";
+import height from "../../../assets/images/icons/height.svg";
+import bulbasaur from "../../../assets/images/pokemons/bulbasaur.png";
 
 const TypeChip = ({ type }) => {
   const styles = {
@@ -44,12 +47,30 @@ const PokemonCard = (props) => {
         <span className="pokemon-number">#001</span>
       </div>
       <div className="pokemon-attributes">
+        <img src={bulbasaur} alt="" className="poke-image"></img>
         <PokemonTypesChips types={types} />
         <AboutTitle type={types[0]} />
         <div className="pokemon-data">
-          <div>prop 1</div>
-          <div>prop 2</div>
+          <div className="data-group">
+            <div className="title-data-group">
+              <img src={weight} width={16} height={16} alt="weight icon" />
+              <p>6,9 kg</p>
+            </div>
+            <span className="data-group-subtitle">Weight</span>
+          </div>
+          <div className="divider"></div>
+          <div className="data-group">
+            <div className="title-data-group">
+              <img src={height} width={16} height={16} alt="height icon" />
+              <p>0,7 m</p>
+            </div>
+            <span className="data-group-subtitle">Height</span>
+          </div>
         </div>
+        <p className="pokemon-description">
+          There is a plant seed on its back right from the day this Pokémon is
+          born.
+        </p>
       </div>
     </article>
   );
