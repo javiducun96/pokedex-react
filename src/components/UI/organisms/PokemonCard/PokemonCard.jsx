aca7664fe8b5f10b779af0638e8e5e292cc2d8bd
javiducun <javiducun@gmail.com>
2023-05-16 18:29:04 +0200

    fix: maintype color in card

--- src/components/UI/organisms/PokemonCard/PokemonCard.jsx
+++ src/components/UI/organisms/PokemonCard/PokemonCard.jsx
@@ -2,11 +2,12 @@ import { PokemonAtributtes } from "../../molecules/PokemonAtributtes"
 import "./styles.css"
 
 const PokemonCard = ({ pokemon }) => {
+  const mainType = pokemon?.types[0]
   return (
-    <article className="card type-grass">
+    <article className={`card type-${mainType}`}>
       <div className="card-header">
-        <span className="pokemon-name">{pokemon.name}</span>
-        <span className="pokemon-number">{pokemon.number}</span>
+        <span className="pokemon-name">{pokemon?.name}</span>
+        <span className="pokemon-number">{pokemon?.number}</span>
       </div>
       <PokemonAtributtes pokemon={pokemon} />
     </article>
