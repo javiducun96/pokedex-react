702bf6c228bf03f8ac53745628564f5b8533721e
javiducun <javiducun@gmail.com>
2023-05-16 18:06:08 +0200

    change label names

--- src/components/UI/molecules/PokemonAtributtes/PokemonAtributtes.jsx
+++ src/components/UI/molecules/PokemonAtributtes/PokemonAtributtes.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import "./styles.css"
-import weight from "../../../../assets/images/icons/width.svg"
-import height from "../../../../assets/images/icons/height.svg"
-import bulbasaur from "../../../../assets/images/pokemons/bulbasaur.png"
+import weightPhoto from "../../../../assets/images/icons/width.svg"
+import heightPhoto from "../../../../assets/images/icons/height.svg"
+import bulbasaurPhoto from "../../../../assets/images/pokemons/bulbasaur.png"
 
 import { ChipGroup } from "../../molecules/ChipGroup"
 import { AboutTitle } from "../../atoms/AboutTitle/AboutTitle"
@@ -10,13 +10,13 @@ import { AboutTitle } from "../../atoms/AboutTitle/AboutTitle"
 export const PokemonAtributtes = ({ pokemon }) => {
   return (
     <div className="pokemon-attributes">
-      <img src={bulbasaur} alt="" className="poke-image"></img>
+      <img src={bulbasaurPhoto} alt="" className="poke-image"></img>
       <ChipGroup types={pokemon.types} />
       <AboutTitle type={pokemon.types[0]} />
       <div className="pokemon-data">
         <div className="data-group">
           <div className="title-data-group">
-            <img src={weight} width={16} height={16} alt="weight icon" />
+            <img src={weightPhoto} width={16} height={16} alt="weight icon" />
             <p>{pokemon.weight} kg</p>
           </div>
           <span className="data-group-subtitle">Weight</span>
@@ -24,7 +24,7 @@ export const PokemonAtributtes = ({ pokemon }) => {
         <div className="divider"></div>
         <div className="data-group">
           <div className="title-data-group">
-            <img src={height} width={16} height={16} alt="height icon" />
+            <img src={heightPhoto} width={16} height={16} alt="height icon" />
             <p>{pokemon.height} m</p>
           </div>
           <span className="data-group-subtitle">Height</span>
