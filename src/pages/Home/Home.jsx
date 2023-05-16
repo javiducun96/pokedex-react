5105da927e7a3de3b3a8979369e72a04cf9a0c17
javiducun <javiducun@gmail.com>
2023-05-16 19:40:14 +0200

    add: more pokemons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,12 +7,6 @@ import gastlyPhoto from "../../assets/images/pokemons/gastly.png"
 import pikachuPhoto from "../../assets/images/pokemons/pikachu.png"
 import squirtlePhoto from "../../assets/images/pokemons/squirtle.png"
 
-function getPokemonCard(pokemon) {
-  return <PokemonCard pokemon={pokemon} />
-}
-
-const getPokemoncard2 = (pokemon) => <PokemonCard pokemon={pokemon} />
-
 export const Home = () => {
   const pokemons = [
     {
@@ -34,6 +28,46 @@ export const Home = () => {
       description:
         "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
       image: charmanderPhoto,
+    },
+    {
+      name: "Squitle",
+      number: "#007",
+      types: ["water"],
+      weight: 9.0,
+      height: 0.5,
+      description:
+        "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+      image: squirtlePhoto,
+    },
+    {
+      name: "Bulbasaur",
+      number: "#001",
+      types: ["grass", "poison"],
+      weight: 6.9,
+      height: 0.7,
+      description:
+        "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+      image: bulbasaurPhoto,
+    },
+    {
+      name: "Charmander",
+      number: "#004",
+      types: ["fire"],
+      weight: 8.5,
+      height: 0.6,
+      description:
+        "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+      image: charmanderPhoto,
+    },
+    {
+      name: "Squitle",
+      number: "#007",
+      types: ["water"],
+      weight: 9.0,
+      height: 0.5,
+      description:
+        "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+      image: squirtlePhoto,
     },
   ]
   return (
