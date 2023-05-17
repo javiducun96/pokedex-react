9aa58b9a710d2ff792d6335ac858c4a2e2cf6d79
javiducun <javiducun@gmail.com>
2023-05-17 21:48:50 +0200

    change: array on N pokemons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,7 @@ import "./styles.css"
 import { PokemonList } from "../../components/UI/organisms/PokemonList"
 
 export const Home = () => {
-  const pokemons = [
-    1,2,3,4,5,6,7,8,9,10
-  ]
+  const pokemons = Array.from({length: 550}, (_, i) => i + 1)
 
   return <PokemonList pokemons={pokemons} />
 }
